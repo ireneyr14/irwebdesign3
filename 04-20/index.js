f0cf71d1136b6518d26aff42456d25a792f2a442
ireneyr14 <61213406+ireneyr14@users.noreply.github.com>
2020-04-20 23:42:48 -0700

    Add files via upload

---
+++ 04-20/index.js
@@ -0,0 +1,19 @@
+$('#carouselControl').click(function () {
+
+	if ($('#carouselControl').hasClass('paused')) {
+		$('#myCarousel').carousel('cycle');
+		$('#carouselControl').text('Pause');
+	} else {
+		$('#myCarousel').carousel('pause');
+		$('#carouselControl').text('Play');
+	}
+
+	$('#carouselControl').toggleClass('paused');
+});
+
+
+
+
+$('#myCarousel').carousel({
+	interval: 3000,
+})
