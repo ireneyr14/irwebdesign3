1e8e9ea21e2c35b590a314a34793eb3e8c77e436
ireneyr14 <61213406+ireneyr14@users.noreply.github.com>
2020-05-18 02:22:49 -0700

    irwebdesign3

--- 05-18/js/canvas.js
+++ 05-18/js/canvas.js
@@ -1,9 +1,8 @@
-
 var canvas = document.getElementById("Game");
 var ctx = canvas.getContext("2d");
 
-var ballX = canvas.width/2;
-var ballY = canvas.height/2;
+var ballX = canvas.width / 2;
+var ballY = canvas.height / 2;
 var ballColor = 'rgb(235, 104, 91)';
 var ballColorG = 'rgb(91,235,104)';
 var ballRadius = 50;
@@ -15,38 +14,36 @@ var directionDown = true;
 var directionRight = true;
 
 
-function animate(){
+function animate() {
     //ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     ctx.beginPath();
-    ctx.fillStyle =  ballColor;
-    ctx.strokeStyle = "#" +  Math.floor(Math.random()*0xFFFFFF).toString(16);
-    ctx.arc(ballX, ballY, ballRadius, 0, 2*Math.PI);
+    ctx.fillStyle = ballColor;
+    ctx.strokeStyle = "#" + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+    ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
     ctx.fill();
     ctx.stroke();
     ctx.closePath();
 
-    if(ballX + ballRadius >= canvas.width || ballX - ballRadius <= 0 ) {
+    if (ballX + ballRadius >= canvas.width || ballX - ballRadius <= 0) {
         directionRight = !directionRight;
     }
 
-    if(directionRight){
+    if (directionRight) {
         ballX = ballX + speedX;
-    }
-    else {
+    } else {
         ballX = ballX - speedX;
-    } 
+    }
 
-    if(ballY + ballRadius >= canvas.height || ballY - ballRadius <= 0) {
+    if (ballY + ballRadius >= canvas.height || ballY - ballRadius <= 0) {
         directionDown = !directionDown;
     }
-    if(directionDown){
+    if (directionDown) {
         ballY = ballY + speedY;
-    }
-    else {
+    } else {
         ballY = ballY - speedY;
     }
 
@@ -57,39 +54,39 @@ animate();
 
 // SCORE
 
-canvas.addEventListener("click", function(event){
+canvas.addEventListener("click", function (event) {
 
     var distX = Math.abs(ballX - event.offsetX);
     var distY = Math.abs(ballY - event.offsetY);
 
-    if(distX < ballRadius && distY < ballRadius){
-        document.getElementById("score-title").innerHTML = distX + distY ;
-        
+    if (distX < ballRadius && distY < ballRadius) {
+        document.getElementById("score-title").innerHTML = distX + distY;
+
     }
-  
-    
+
+
 })
 
 // BALL COLOR CHANGE GREEN
 
 function makeGreen(color) {
     ballColor = color;
-  }
+}
 
 // BALL COLOR CHANGE PURPLE
 
 function makePurple(color) {
     ballColor = color;
-  }
+}
 
 // BALL COLOR CHANGE BLUE
 
 function makeBlue(color) {
     ballColor = color;
-  }
+}
 
-  // BALL COLOR CHANGE YELLOW
+// BALL COLOR CHANGE YELLOW
 
 function makeYellow(color) {
     ballColor = color;
-  }
+}
